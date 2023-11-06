4bb2e736a5badc281431dce5e834d581bb24c11e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-06 20:16:17 +0200

    Add files via upload

---
+++ 01. Inheritance - Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomStack;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        StackOfStrings stack = new StackOfStrings();
+
+        Console.WriteLine(stack.IsEmpty());
+
+        stack.AddRange(new string[] { "1", "2", "3" });
+
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+    }
+}
