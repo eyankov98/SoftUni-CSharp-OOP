e5f8d200ca20d12148c51ba48b63c08c577a2a5a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-08 22:31:31 +0200

    Add files via upload

---
+++ 02. Inheritance - Exercise/06. Animals/Kitten.cs
@@ -0,0 +1,13 @@
+﻿namespace Animals;
+
+public class Kitten : Cat
+{
+    private const string kittenGender = "Female";
+
+    public Kitten(string name, int age) : base(name, age, kittenGender)
+    {
+
+    }
+
+    public override string ProduceSound() => "Meow";
+}
---
+++ 02. Inheritance - Exercise/06. Animals/Tomcat.cs
@@ -0,0 +1,13 @@
+﻿namespace Animals;
+
+public class Tomcat : Cat
+{
+    private const string tomcatGender = "Female";
+
+    public Tomcat(string name, int age) : base(name, age, tomcatGender)
+    {
+
+    }
+
+    public override string ProduceSound() => "MEOW";
+}
