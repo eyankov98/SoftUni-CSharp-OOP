1632322bc4f4d2588c63e48743938c8e94c76b80
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-09 22:13:59 +0200

    Add files via upload

---
+++ 03. Encapsulation - Lab/01. Persons/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace PersonsInfo;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int peopleCount = int.Parse(Console.ReadLine());
+
+        List<Person> people = new List<Person>();
+
+        for (int i = 0; i < peopleCount; i++)
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstName = personInfo[0];
+            string lastName = personInfo[1];
+            int age = int.Parse(personInfo[2]);
+
+            Person person = new Person(firstName, lastName, age);
+            people.Add(person);
+        }
+
+        foreach (Person person in people.OrderBy(p => p.FirstName))
+        {
+            Console.WriteLine(person);
+        }
+    }
+}
