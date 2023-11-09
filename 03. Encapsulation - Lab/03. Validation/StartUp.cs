bac09ee401d96655493b099436d06e5c50168fe9
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-09 22:14:50 +0200

    Add files via upload

---
+++ 03. Encapsulation - Lab/03. Validation/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace PersonsInfo;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int countPerson = int.Parse(Console.ReadLine());
+        List<Person> people = new List<Person>();
+
+        for (int i = 0; i < countPerson; i++)
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstName = personInfo[0];
+            string lastName = personInfo[1];
+            int age = int.Parse(personInfo[2]);
+            decimal salary = decimal.Parse(personInfo[3]);
+
+            try
+            {
+                Person person = new Person(firstName, lastName, age, salary);
+
+                people.Add(person);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        decimal percentage = decimal.Parse(Console.ReadLine());
+
+        foreach (var person in people)
+        {
+            person.IncreaseSalary(percentage);
+
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
