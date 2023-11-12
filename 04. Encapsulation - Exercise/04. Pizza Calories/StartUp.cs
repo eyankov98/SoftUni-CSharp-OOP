895d4d8521367f5be2569791840df177bb07acd3
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-12 11:41:36 +0200

    Add files via upload

---
+++ 04. Encapsulation - Exercise/04. Pizza Calories/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace PizzaCalories;
+
+using PizzaCalories.Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string[] pizzaInputInfo = Console.ReadLine()
+            .Split(" ");
+
+            string pizzaName = pizzaInputInfo[1];
+
+            string[] doughInputInfo = Console.ReadLine()
+                .Split(" ");
+
+            string flourType = doughInputInfo[1];
+            string bakingTechnique = doughInputInfo[2];
+            double weightDough = double.Parse(doughInputInfo[3]);
+
+            Dough dough = new Dough(weightDough, flourType, bakingTechnique);
+
+            Pizza pizza = new Pizza(pizzaName, dough);
+
+            string toppingsInputInfo = string.Empty;
+
+            while ((toppingsInputInfo = Console.ReadLine()) != "END")
+            {
+                string[] toppingsInfo = toppingsInputInfo
+                    .Split(" ");
+
+                string toppingName = toppingsInfo[1];
+                double weightTopping = double.Parse(toppingsInfo[2]);
+
+                Topping topping = new Topping(toppingName, weightTopping);
+
+                pizza.AddTopping(topping);
+            }
+
+            Console.WriteLine(pizza);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
