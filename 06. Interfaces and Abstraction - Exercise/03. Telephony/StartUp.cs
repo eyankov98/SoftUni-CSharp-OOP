a7e1226a1047387fd0cdfa169dcad7b0f4314b54
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-17 20:45:26 +0200

    Add files via upload

---
+++ 06. Interfaces and Abstraction - Exercise/03. Telephony/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Telephony.Core;
+using Telephony.Core.Interfaces;
+using Telephony.Models;
+using Telephony.Models.Interfaces;
+using Telephony.IO;
+
+namespace Telephony;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        IEngine engine = new Engine(new ConsoleReader(), new ConsoleWriter());
+
+        engine.Run();
+    }
+}
