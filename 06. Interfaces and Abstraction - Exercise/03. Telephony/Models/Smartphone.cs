ef1bc2730cb6c3b3674e6da0ae9b45818209977e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-17 20:44:48 +0200

    Add files via upload

---
+++ 06. Interfaces and Abstraction - Exercise/03. Telephony/Models/Smartphone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telephony.Models.Interfaces;
+
+namespace Telephony.Models
+{
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public string Call(string phoneNumber)
+        {
+            if (!ValidatePhoneNumber(phoneNumber))
+            {
+                throw new ArgumentException("Invalid number!");
+            }
+
+            return $"Calling... {phoneNumber}";
+        }
+
+        public string Browse(string url)
+        {
+            if (!ValidateUrl(url))
+            {
+                throw new ArgumentException("Invalid URL!");
+            }
+
+            return $"Browsing: {url}!";
+        }
+
+        public bool ValidatePhoneNumber(string phoneNumber) 
+            => phoneNumber.All(ch => char.IsDigit(ch)); // return phoneNumber.All(ch => char.IsDigit(ch));
+
+        public bool ValidateUrl(string url)
+            => url.All(ch => !char.IsDigit(ch));
+    }
+}
