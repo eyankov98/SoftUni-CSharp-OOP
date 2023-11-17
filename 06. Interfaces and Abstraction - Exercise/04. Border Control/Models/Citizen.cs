1d71074299f6974ea6a35e11ade24b2a5d99a9c9
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-17 20:48:05 +0200

    Add files via upload

---
+++ 06. Interfaces and Abstraction - Exercise/04. Border Control/Models/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using BorderControl.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderControl.Models
+{
+    public class Citizen : IIdentifiable
+    {
+        public Citizen(string name, int age, string id)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public string Id { get; private set; }
+    }
+}
