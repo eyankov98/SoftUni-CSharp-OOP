7adb312aae2bc6e4502346141fcf6dfdd6628f62
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-18 22:08:46 +0200

    Add files via upload

---
+++ 06. Interfaces and Abstraction - Exercise/06. Food Shortage/Models/Citizen.cs
@@ -0,0 +1,33 @@
+﻿using FoodShortage.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodShortage.Models
+{
+    public class Citizen : INameable, IIdentifiable, IBirthable, IBuyer
+    {
+        private const int DefaultFoodIncrement = 10;
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+
+        public string Name { get; private set; } 
+        public int Age { get; private set; }
+        public string Id { get; private set; } 
+        public string Birthdate { get; private set; } 
+        public int Food { get; private set; } 
+
+    public void BuyFood()
+        {
+            Food += DefaultFoodIncrement;
+        }
+    }
+}
