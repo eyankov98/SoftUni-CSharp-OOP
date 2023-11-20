36a92f06b152d15465285b96cab91187c1e066f4
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-20 23:28:45 +0200

    Add files via upload

---
+++ 07. Polymorphism - Lab/03. Shapes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public int Height 
+        { 
+            get => height; 
+            private set => height = value; 
+        }
+        public int Width 
+        { 
+            get => width; 
+            private set => width = value; 
+        }
+
+        public override double CalculateArea()
+        {
+            return this.height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (this.height * 2) + (this.width * 2);
+        }
+    }
+}
---
+++ 07. Polymorphism - Lab/03. Shapes/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Shapes;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(10, 15);
+
+        Console.WriteLine(circle.Draw());
+        Console.WriteLine(rectangle.Draw());
+    }
+}
