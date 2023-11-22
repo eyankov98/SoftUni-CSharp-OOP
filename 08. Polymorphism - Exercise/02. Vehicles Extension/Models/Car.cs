635cd8a8c32fcdd9cd8f187bfaff4e22c1042f27
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-22 22:07:52 +0200

    Add files via upload

---
+++ 08. Polymorphism - Exercise/02. Vehicles Extension/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclesExtension.Models.Interfaces;
+
+namespace VehiclesExtension.Models
+{
+    public class Car : Vehicle
+    {
+        private const double IncreasedConsumption = 0.9;
+
+        public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) 
+            : base(fuelQuantity, fuelConsumption, tankCapacity, IncreasedConsumption)
+        {
+        }
+    }
+}
