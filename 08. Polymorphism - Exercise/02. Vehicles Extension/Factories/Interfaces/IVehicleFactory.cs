887ffce55c23942454e4ab26a315bba5268777a6
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-22 22:05:25 +0200

    Add files via upload

---
+++ 08. Polymorphism - Exercise/02. Vehicles Extension/Factories/Interfaces/IVehicleFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclesExtension.Models.Interfaces;
+
+namespace VehiclesExtension.Factories.Interfaces
+{
+    public interface IVehicleFactory
+    {
+        IVehicle Create(string typeOfVehicle, double fuelQuantity, double fuelConsumption, double tankCapacity);
+    }
+}
