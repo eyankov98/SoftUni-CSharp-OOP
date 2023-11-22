a5163583aca4b16312bad8da0fa02a89c081402c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-22 22:05:49 +0200

    Add files via upload

---
+++ 08. Polymorphism - Exercise/02. Vehicles Extension/Factories/VehicleFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclesExtension.Factories.Interfaces;
+using VehiclesExtension.Models;
+using VehiclesExtension.Models.Interfaces;
+
+namespace VehiclesExtension.Factories
+{
+    public class VehicleFactory : IVehicleFactory
+    {
+        public IVehicle Create(string typeOfVehicle, double fuelQuantity, double fuelConsumption, double tankCapacity)
+        {
+            switch (typeOfVehicle)
+            {
+                case "Car":
+                    return new Car(fuelQuantity, fuelConsumption, tankCapacity);
+                case "Truck":
+                    return new Truck(fuelQuantity, fuelConsumption, tankCapacity);
+                case "Bus":
+                    return new Bus(fuelQuantity, fuelConsumption, tankCapacity);
+                default:
+                    throw new ArgumentException("Invalid vehicle type");
+            }
+        }
+    }
+}
