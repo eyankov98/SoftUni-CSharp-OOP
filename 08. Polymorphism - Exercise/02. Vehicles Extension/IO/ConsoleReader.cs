98b1ffe3dd3c3307b7c9c7c7fcd554e2801879d1
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-22 22:06:51 +0200

    Add files via upload

---
+++ 08. Polymorphism - Exercise/02. Vehicles Extension/IO/ConsoleReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclesExtension.IO.Interfaces;
+
+namespace VehiclesExtension.IO
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine() => Console.ReadLine();
+    }
+}
---
+++ 08. Polymorphism - Exercise/02. Vehicles Extension/IO/ConsoleWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclesExtension.IO.Interfaces;
+
+namespace VehiclesExtension.IO
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void WriteLine(string text) => Console.WriteLine(text);
+
+    }
+}
