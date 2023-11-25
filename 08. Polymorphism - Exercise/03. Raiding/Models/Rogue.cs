6e345fd77bb823bb81ba171306be8e2115ff3e0f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-25 21:54:14 +0200

    Add files via upload

---
+++ 08. Polymorphism - Exercise/03. Raiding/Models/Rogue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Models
+{
+    public class Rogue : Hero
+    {
+        private const int RogueDefaultPower = 80;
+
+        public Rogue(string name) 
+            : base(name, RogueDefaultPower)
+        {
+        }
+
+        public override string CastAbility()
+            => $"{this.GetType().Name} - {Name} hit for {Power} damage";
+    }
+}
---
+++ 08. Polymorphism - Exercise/03. Raiding/Models/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Models
+{
+    public class Warrior : Hero
+    {
+        private const int WarriorDefaultPower = 100;
+        public Warrior(string name) 
+            : base(name, WarriorDefaultPower)
+        {
+        }
+
+        public override string CastAbility() 
+            => $"{this.GetType().Name} - {Name} hit for {Power} damage";
+    }
+}
