bf5a7643bbd3ade4408508ec3c4f9929a08188d4
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-28 23:41:50 +0200

    Add files via upload

---
+++ 09. Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿List<int> numbers = new List<int>();
+
+while (numbers.Count != 10)
+{
+
+    try
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if (number <= 1 || number >= 100)
+        {
+            int lastNumber = numbers.LastOrDefault() == 0
+                 ? 1
+                 : numbers.LastOrDefault();
+
+            throw new ArgumentOutOfRangeException("", $"Your number is not in range {lastNumber} - 100!");
+        }
+        else if (numbers.Count == 0)
+        {
+            numbers.Add(number);
+        }
+        else if (numbers.LastOrDefault() < number)
+        {
+            numbers.Add(number);
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("", $"Your number is not in range {numbers.LastOrDefault()} - 100!");
+        }
+
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Invalid Number!");
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+Console.WriteLine(string.Join(", ", numbers));
