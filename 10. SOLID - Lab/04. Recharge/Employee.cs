479fa9610bfbfff410f02be55f2fb975c2c19796
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-29 23:18:45 +0200

    Add files via upload

---
+++ 10. SOLID - Lab/04. Recharge/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Recharge;
+
+public class Employee : Worker, ISleeper
+{
+    public Employee(string id) : base(id)
+    {
+    }
+
+    public void Sleep()
+    {
+        // sleep...
+    }
+}
