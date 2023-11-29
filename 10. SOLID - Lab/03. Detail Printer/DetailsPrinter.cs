53c257ba83e949f225494e642c79d6de48a6bc3e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-11-29 23:18:14 +0200

    Add files via upload

---
+++ 10. SOLID - Lab/03. Detail Printer/DetailsPrinter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace DetailPrinter;
+
+public class DetailsPrinter
+{
+    private IList<Employee> employees;
+
+    public DetailsPrinter(IList<Employee> employees)
+    {
+        this.employees = employees;
+    }
+
+    public void PrintDetails()
+    {
+        foreach (Employee employee in this.employees)
+        {
+            Console.WriteLine(employee.ToString());
+        }
+    }
+
+    private void PrintEmployee(Employee employee)
+    {
+        Console.WriteLine(employee.Name);
+    }
+
+    private void PrintManager(Manager manager)
+    {
+        Console.WriteLine(manager.Name);
+        Console.WriteLine(string.Join(Environment.NewLine, manager.Documents));
+    }
+}
---
+++ 10. SOLID - Lab/03. Detail Printer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DetailPrinter;
+
+class Program
+{
+    static void Main()
+    {
+        Employee employee = new Employee("John The Employee");
+        ICollection<string> documents = new List<string>()
+        {
+            "Document One",
+            "Document Two",
+            "Document Three"
+        };
+        Employee manager = new Manager("Peter The Manager", documents);
+
+        List<Employee> employees = new List<Employee>();
+        employees.Add(employee);
+        employees.Add(manager);
+
+        DetailsPrinter printer = new DetailsPrinter(employees);
+        printer.PrintDetails();
+    }
+}
