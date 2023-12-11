c83ddfc1b548c01baa7420049f8acb362f789972
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-12-12 00:04:14 +0200

    Add files via upload

---
+++ 14. Unit Testing - Lab/02. Dummy Tests/AxeTests.cs
@@ -0,0 +1,52 @@
+using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeShouldInitlizeWithCorrectValues()
+        {
+            // Arrange and Act
+            Axe axe = new Axe(100, 100);
+
+            //Assert
+            Assert.AreEqual(100, axe.DurabilityPoints);
+            Assert.AreEqual(100, axe.AttackPoints);
+
+        }
+
+        [Test]
+        public void AttackMethodShouldDecreaseDurabilityPoints()
+        {
+            //Arrange
+            Dummy target = new Dummy(10, 10);
+            Axe axe = new Axe(100, 10);
+
+            //Act
+            axe.Attack(target);
+
+            //Assert
+            Assert.AreEqual(9, axe.DurabilityPoints);
+        }
+
+        [Test]
+        public void AttackMethodShouldThrowAnExceptionIfDurabilityIsZero()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(100, 100);
+            Axe axe = new Axe(10, 10);
+
+            //Act
+            for (int i = 0; i < 10; i++)
+            {
+                axe.Attack(dummy);
+            }
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy), "Axe is broken.");
+        }
+    }
+}
