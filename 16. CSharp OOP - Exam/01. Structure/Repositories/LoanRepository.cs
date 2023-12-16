fba70461d2480f899b7e4910aa21169d3da15aa1
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-12-16 19:20:09 +0200

    Add files via upload

---
+++ 16. CSharp OOP - Exam/01. Structure/Repositories/LoanRepository.cs
@@ -0,0 +1,33 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoan.Repositories
+{
+    public class LoanRepository : IRepository<ILoan>
+    {
+        private List<ILoan> loans;
+
+        public LoanRepository()
+        {
+            this.loans = new List<ILoan>();
+        }
+
+        public IReadOnlyCollection<ILoan> Models => this.loans;
+
+        public void AddModel(ILoan model)
+        {
+            this.loans.Add(model);
+        }
+
+        public bool RemoveModel(ILoan model)
+            => this.loans.Remove(model);
+
+        public ILoan FirstModel(string name)
+            => this.loans.FirstOrDefault(l => l.GetType().Name == name);
+    }
+}
