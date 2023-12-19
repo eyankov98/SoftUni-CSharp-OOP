e83131c5abbcf84dba64338a397c42fea1d3cc8a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-12-19 17:04:02 +0200

    Add files via upload

---
+++ 16. CSharp OOP - Exam/02. Business Logic/Repositories/BankRepository.cs
@@ -0,0 +1,33 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoan.Repositories
+{
+    public class BankRepository : IRepository<IBank>
+    {
+        private List<IBank> banks;
+
+        public BankRepository()
+        {
+            this.banks = new List<IBank>();
+        }
+
+        public IReadOnlyCollection<IBank> Models => this.banks;
+
+        public void AddModel(IBank model)
+        {
+            this.banks.Add(model);
+        }
+
+        public bool RemoveModel(IBank model)
+            => this.banks.Remove(model);
+
+        public IBank FirstModel(string name)
+            => this.banks.FirstOrDefault(b => b.Name == name);
+    }
+}
