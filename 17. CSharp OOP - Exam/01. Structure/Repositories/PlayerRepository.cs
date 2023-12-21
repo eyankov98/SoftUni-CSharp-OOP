e799f03a7264cf6cdfcf4b58dc82792741727d13
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-12-21 23:48:59 +0200

    Add files via upload

---
+++ 17. CSharp OOP - Exam/01. Structure/Repositories/PlayerRepository.cs
@@ -0,0 +1,36 @@
+﻿using Handball.Models.Contracts;
+using Handball.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Repositories
+{
+    public class PlayerRepository : IRepository<IPlayer>
+    {
+        private List<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+        }
+
+        public IReadOnlyCollection<IPlayer> Models => this.players;
+
+        public void AddModel(IPlayer model)
+        {
+            this.players.Add(model);
+        }
+
+        public bool RemoveModel(string name)
+            => this.players.Remove(this.players.FirstOrDefault(p => p.Name == name));
+
+        public bool ExistsModel(string name)
+            => this.players.Any(p => p.Name == name);
+
+        public IPlayer GetModel(string name)
+            => this.players.FirstOrDefault(p => p.Name == name);
+    }
+}
---
+++ 17. CSharp OOP - Exam/01. Structure/Repositories/TeamRepository.cs
@@ -0,0 +1,36 @@
+﻿using Handball.Models.Contracts;
+using Handball.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Repositories
+{
+    public class TeamRepository : IRepository<ITeam>
+    {
+        private List<ITeam> teams;
+
+        public TeamRepository()
+        {
+            this.teams = new List<ITeam>();
+        }
+
+        public IReadOnlyCollection<ITeam> Models => this.teams;
+
+        public void AddModel(ITeam model)
+        {
+            this.teams.Add(model);
+        }
+
+        public bool RemoveModel(string name)
+            => this.teams.Remove(this.teams.FirstOrDefault(t => t.Name == name));
+
+        public bool ExistsModel(string name)
+            => this.teams.Any(t => t.Name == name);
+
+        public ITeam GetModel(string name)
+            => this.teams.FirstOrDefault(t => t.Name == name);
+    }
+}
