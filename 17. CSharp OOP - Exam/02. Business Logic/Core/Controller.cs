b2aebfb61307db93c3105beece3886bf2fcb19e5
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-12-22 10:50:01 +0200

    Add files via upload

---
+++ 17. CSharp OOP - Exam/02. Business Logic/Core/Controller.cs
@@ -0,0 +1,157 @@
+﻿using Handball.Core.Contracts;
+using Handball.Models;
+using Handball.Models.Contracts;
+using Handball.Repositories;
+using Handball.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Core
+{
+    public class Controller : IController
+    {
+        private PlayerRepository players;
+        private TeamRepository teams;
+
+        public Controller()
+        {
+            this.players = new PlayerRepository();
+            this.teams = new TeamRepository();
+        }
+
+        public string NewTeam(string name)
+        {
+            if (this.teams.ExistsModel(name))
+            {
+                return string.Format(OutputMessages.TeamAlreadyExists, name, nameof(TeamRepository));
+            }
+
+            ITeam team = new Team(name);
+
+            this.teams.AddModel(team);
+
+            return string.Format(OutputMessages.TeamSuccessfullyAdded, name, nameof(TeamRepository));
+        }
+
+        public string NewPlayer(string typeName, string name)
+        {
+            if (typeName != nameof(Goalkeeper) && typeName != nameof(CenterBack) && typeName != nameof(ForwardWing))
+            {
+                return string.Format(OutputMessages.InvalidTypeOfPosition, typeName);
+            }
+
+            if (this.players.ExistsModel(name))
+            {
+                return string.Format(OutputMessages.PlayerIsAlreadyAdded, name, nameof(PlayerRepository), this.players.GetModel(name).GetType().Name);
+            }
+
+            IPlayer player;
+
+            if (typeName == nameof(Goalkeeper))
+            {
+                player = new Goalkeeper(name);
+            }
+            else if (typeName == nameof(CenterBack))
+            {
+                 player = new CenterBack(name);
+            }
+            else // typeName == nameof(ForwardWing)
+            {
+                player = new ForwardWing(name);
+            }
+
+            this.players.AddModel(player);
+
+            return string.Format(OutputMessages.PlayerAddedSuccessfully, name);
+        }
+
+        public string NewContract(string playerName, string teamName)
+        {
+            IPlayer player = this.players.GetModel(playerName);
+
+            if (player == null)
+            {
+                return string.Format(OutputMessages.PlayerNotExisting, playerName, nameof(PlayerRepository));
+            }
+
+            ITeam team = this.teams.GetModel(teamName);
+
+            if (team == null)
+            {
+                return string.Format(OutputMessages.TeamNotExisting, teamName, nameof(TeamRepository));
+            }
+
+            if (player.Team != null)
+            {
+                return string.Format(OutputMessages.PlayerAlreadySignedContract, playerName, player.Team);
+            }
+
+            player.JoinTeam(teamName);
+
+            team.SignContract(player);
+
+            return string.Format(OutputMessages.SignContract, playerName, teamName);
+        }
+
+        public string NewGame(string firstTeamName, string secondTeamName)
+        {
+            ITeam firstTeam = this.teams.GetModel(firstTeamName);
+            ITeam secondTeam = this.teams.GetModel(secondTeamName);
+
+            if (firstTeam.OverallRating > secondTeam.OverallRating)
+            {
+                firstTeam.Win();
+                secondTeam.Lose();
+
+                return string.Format(OutputMessages.GameHasWinner, firstTeamName, secondTeamName);
+            }
+            else if (firstTeam.OverallRating < secondTeam.OverallRating)
+            {
+                secondTeam.Win();
+                firstTeam.Lose();
+
+                return string.Format(OutputMessages.GameHasWinner, secondTeamName, firstTeamName);
+            }
+            else 
+            {
+                firstTeam.Draw();
+                secondTeam.Draw();
+
+                return string.Format(OutputMessages.GameIsDraw, firstTeamName, secondTeamName);
+            }
+        }
+
+        public string PlayerStatistics(string teamName)
+        {
+            ITeam team = this.teams.GetModel(teamName);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"***{teamName}***");
+
+            foreach (var player in team.Players.OrderByDescending(p => p.Rating).ThenBy(p => p.Name))
+            {
+                sb.AppendLine(player.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string LeagueStandings()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("***League Standings***");
+
+            foreach (var team in this.teams.Models.OrderByDescending(t => t.PointsEarned).ThenByDescending(t => t.OverallRating).ThenBy(t => t.Name))
+            {
+                sb.AppendLine(team.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
