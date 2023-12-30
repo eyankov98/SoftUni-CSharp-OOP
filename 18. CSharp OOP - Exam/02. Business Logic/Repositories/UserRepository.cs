e3957dd7cd2322131e760686c407fdba42ffda71
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-12-30 20:45:24 +0200

    Add files via upload

---
+++ 18. CSharp OOP - Exam/02. Business Logic/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Repositories
+{
+    public class UserRepository : IRepository<IUser>
+    {
+        private List<IUser> users;
+
+        public UserRepository()
+        {
+            this.users = new List<IUser>();
+        }
+
+        public void AddModel(IUser model)
+        {
+            this.users.Add(model);
+        }
+
+        public IUser FindById(string identifier) 
+            => this.users.FirstOrDefault(u => u.DrivingLicenseNumber == identifier);
+
+        public IReadOnlyCollection<IUser> GetAll() 
+            => this.users;
+
+        public bool RemoveById(string identifier)
+            => this.users.Remove(FindById(identifier));
+    }
+}
