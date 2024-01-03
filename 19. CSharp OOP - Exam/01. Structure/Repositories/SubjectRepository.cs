088f4410741616e5f7ae52a2f30efded068165ab
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-03 22:40:16 +0200

    Add files via upload

---
+++ 19. CSharp OOP - Exam/01. Structure/Repositories/SubjectRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories
+{
+    public class SubjectRepository : IRepository<ISubject>
+    {
+        private List<ISubject> models;
+
+        public SubjectRepository()
+        {
+            this.models = new List<ISubject>();
+        }
+
+        public IReadOnlyCollection<ISubject> Models => this.models;
+
+        public void AddModel(ISubject model)
+        {
+            this.models.Add(model);
+        }
+
+        public ISubject FindById(int id)
+            => this.models.FirstOrDefault(m => m.Id == id);
+
+        public ISubject FindByName(string name)
+            => this.models.FirstOrDefault(m => m.Name == name);
+    }
+}
---
+++ 19. CSharp OOP - Exam/01. Structure/Repositories/UniversityRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories
+{
+    public class UniversityRepository : IRepository<IUniversity>
+    {
+        List<IUniversity> universities;
+
+        public UniversityRepository()
+        {
+            universities = new List<IUniversity>();
+        }
+
+        public IReadOnlyCollection<IUniversity> Models => this.universities;
+
+        public void AddModel(IUniversity model)
+        {
+            this.universities.Add(model);
+        }
+
+        public IUniversity FindById(int id)
+            => this.universities.FirstOrDefault(u => u.Id == id);
+
+        public IUniversity FindByName(string name)
+            => this.universities.FirstOrDefault(u => u.Name == name);
+    }
+}
