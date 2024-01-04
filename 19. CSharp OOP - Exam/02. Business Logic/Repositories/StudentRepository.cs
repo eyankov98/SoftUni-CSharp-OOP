bb642d2d47354523349fbf025062ab02ef7d7ccc
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-04 22:06:41 +0200

    Add files via upload

---
+++ 19. CSharp OOP - Exam/02. Business Logic/Repositories/StudentRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories
+{
+    public class StudentRepository : IRepository<IStudent>
+    {
+        private List<IStudent> students;
+
+        public StudentRepository()
+        {
+            this.students = new List<IStudent>();
+        }
+
+        public IReadOnlyCollection<IStudent> Models => this.students;
+
+        public void AddModel(IStudent model)
+        {
+            this.students.Add(model);
+        }
+
+        public IStudent FindById(int id)
+            => this.students.FirstOrDefault(s => s.Id == id);
+
+        public IStudent FindByName(string name)
+            => this.students.FirstOrDefault(s => s.FirstName == name.Split(" ")[0] && s.LastName == name.Split(" ")[1]);
+        
+        /*{
+            string firstName = name.Split(" ")[0];
+            string lastName = name.Split(" ")[1];
+
+            return this.students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName); 
+        }*/
+    }
+}
