b8cf510b79e0fa2b55631fb001a559e75657a01e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-04 22:04:49 +0200

    Add files via upload

---
+++ 19. CSharp OOP - Exam/02. Business Logic/Models/Subject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Utilities.Messages;
+
+namespace UniversityCompetition.Models
+{
+    public abstract class Subject : ISubject
+    {
+        private int id;
+        private string name;
+        private double rate;
+
+        public Subject(int id, string name, double rate)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Rate = rate;
+        }
+
+        public int Id
+        {
+            get => id; 
+            private set => id = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+
+                name = value;
+            }
+        }
+
+        public double Rate
+        {
+            get => rate;
+            private set => rate = value;
+        }
+    }
+}
