a7a3975c58cdc0a0ec7014e7f437520554544a37
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-05 21:21:03 +0200

    Add files via upload

---
+++ 20. CSharp OOP - Exam/01. Structure/Models/Cocktails/Cocktail.cs
@@ -0,0 +1,58 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Cocktails
+{
+    public abstract class Cocktail : ICocktail
+    {
+        private string name;
+        private double price;
+
+        public Cocktail (string name, string size, double price)
+        {
+            this.Name = name;
+            this.Size = size;
+            this.Price = price;
+        }
+
+        public string Name
+        {
+            get => this.name; 
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+
+                this.name = value;
+            }
+        }
+
+        public string Size { get; private set; }
+
+        public double Price
+        {
+            get => this.price;
+            private set
+            {
+                if (Size == "Small")
+                {
+                    value /= 3;
+                }
+                else if (Size == "Middle")
+                {
+                    value = (value / 3) * 2;
+                }
+
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+            => $"{this.Name} ({this.Size}) - {Price:F2} lv";
+    }
+}
