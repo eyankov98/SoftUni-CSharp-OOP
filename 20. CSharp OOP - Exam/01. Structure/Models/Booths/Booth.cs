9bd9b9d52861fec347c367a7bf3c695fe50d72e8
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-05 21:20:03 +0200

    Add files via upload

---
+++ 20. CSharp OOP - Exam/01. Structure/Models/Booths/Booth.cs
@@ -0,0 +1,105 @@
+﻿using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Models.Cocktails;
+using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Repositories;
+using ChristmasPastryShop.Repositories.Contracts;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Booths
+{
+    public class Booth : IBooth
+    {
+        private int capacity;
+
+        private readonly DelicacyRepository delicacies;
+        private readonly CocktailRepository cocktails;
+
+        public Booth(int boothId, int capacity)
+        {
+            this.BoothId = boothId;
+            this.Capacity = capacity;
+
+            this.delicacies = new DelicacyRepository();
+            this.cocktails = new CocktailRepository();
+
+            this.CurrentBill = 0;
+            this.Turnover = 0;
+            this.IsReserved = false;
+        }
+
+        public int BoothId { get; private set; }
+
+        public int Capacity
+        {
+            get => this.capacity;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.CapacityLessThanOne));
+                }
+
+                this.capacity = value;
+            }
+        }
+
+        public IRepository<IDelicacy> DelicacyMenu => this.delicacies;
+
+        public IRepository<ICocktail> CocktailMenu => this.cocktails;
+
+        public double CurrentBill { get; private set; }
+
+        public double Turnover { get; private set; }
+
+        public bool IsReserved { get; private set; }
+
+        public void UpdateCurrentBill(double amount)
+        {
+            this.CurrentBill += amount;
+        }
+
+        public void Charge()
+        {
+            this.Turnover += this.CurrentBill;
+
+            this.CurrentBill = 0;
+        }
+
+        public void ChangeStatus()
+        {
+            if (this.IsReserved)
+            {
+                this.IsReserved = false;
+            }
+            else
+            {
+                this.IsReserved = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Booth: {this.BoothId}");
+            sb.AppendLine($"Capacity: {this.Capacity}");
+            sb.AppendLine($"Turnover: {this.Turnover:F2} lv");
+            sb.AppendLine("-Cocktail menu:");
+            foreach (var cocktail in CocktailMenu.Models)
+            {
+                sb.AppendLine($"--{cocktail}");
+            }
+            sb.AppendLine("-Delicacy menu:");
+            foreach (var delicacy in DelicacyMenu.Models)
+            {
+                sb.AppendLine($"--{delicacy}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
