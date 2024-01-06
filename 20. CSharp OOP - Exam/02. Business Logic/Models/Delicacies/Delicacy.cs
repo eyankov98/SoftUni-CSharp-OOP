c1d34e08bf907783405953a532ad4c19f9e9a8dc
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-01-06 20:04:19 +0200

    Add files via upload

---
+++ 20. CSharp OOP - Exam/02. Business Logic/Models/Delicacies/Delicacy.cs
@@ -0,0 +1,38 @@
+﻿using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Delicacies
+{
+    public abstract class Delicacy : IDelicacy
+    {
+        private string name;
+
+        public Delicacy(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name
+        {
+            get => this.name; 
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+
+                this.name = value;
+            }
+        }
+
+        public double Price { get; private set; }
+
+        public override string ToString()
+            => $"{this.Name} - {this.Price:F2} lv";
+    }
+}
